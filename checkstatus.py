7ebb32c5107c9d3e14254cb70fbb3f4f3cad14b8
Matt Williamson <matt@teltechcorp.com>
2012-04-25 16:18:26 -0400

    Support multiple sites

--- checkstatus.py
+++ checkstatus.py
@@ -8,29 +8,40 @@ SERIAL_PORT = "/dev/tty.usbserial-A70064Mh"
 SERIAL_BAUD = 9600
 
 def usage():
-    print 'Usage: %s url' % __file__
+    print 'Usage: %s url1 [url2] [url3] ...' % __file__
     sys.exit(1)
 
 if __name__ == '__main__':
-    try:
-        url = sys.argv[1]
-    except IndexError:
+    if not len(sys.argv) > 1:
         usage()
 
-    print 'Checking url:', url
-
-    response = requests.get(url)
-
-    print 'Got response code:', response.status_code
-
     s = serial.Serial(SERIAL_PORT, SERIAL_BAUD, timeout=1)
+    any_down = False 
 
-    if response.status_code == 200:
-        print 'Site is up.'
-        s.write('1')
-    else:
-        print 'Site is DOWN!!!'
+    for url in sys.argv[1:]:
+        print '=' * 80
+        print 'Checking url:', url
+
+        try:
+            response = requests.get(url)
+        except:
+            print 'Got exception. Site is down!'
+            any_down = True
+            break
+
+        print 'Got response code:', response.status_code
+
+        if response.status_code != 200:
+            print 'Site is DOWN!!!'
+            any_down = True
+            break
+        else:
+            print 'Site is up.'
+
+    if any_down:
         s.write('0')
+    else:
+        s.write('1')
 
     s.close()
 
